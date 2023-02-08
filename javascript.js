034644acfe9fcc3cfabc1506e737ef916cda72b5
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-02-08 18:28:43 +0100

    Added loop

--- javascript.js
+++ javascript.js
@@ -1,10 +1,42 @@
+//Objekt m. egenskaber.
 let character = {
     firstname: "Codemaster",
     level: "30",
     class: "warrior",
     spec: "protection",
-    hometown: "rts",
+    hometown: "Kolding",
 }
 
-console.log(character.firstname)//dot notation
-console.log(character["level"]) //bracket notation
+//dot notation
+console.log(character.firstname)
+console.log(character.level)
+console.log(character.class)
+console.log(character.spec)
+console.log(character.hometown)
+
+ //bracket notation
+console.log(character["firstname"])
+console.log(character["level"])
+console.log(character["class"])
+console.log(character["spec"])
+console.log(character["hometown"])
+
+
+//opgave 2. Nestede objekter
+let aboutme = {
+    firstname: "Mathias",
+    lastname: "Agzigüzel Sørensen",
+    age: 30,
+    hobbies: ["programming","music","bodybuilding","long walks", "fotography",], //nestede objekter
+    married: true,
+    wifey: {
+        name: "Zeliha",
+        proffession: "Counseling", //Nestede objekter.
+        hobbies: "politics",
+    }
+}
+
+//Løkke som udskriver nestede objekter under hobbies
+aboutme.hobbies.forEach(function(hobby) {
+    console.log(hobby)
+  })
