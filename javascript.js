584665632dc4ab3f5ceac31d1ae92a86a8e54e6c
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2023-02-08 12:31:29 +0100

    Dot & bracket notation added

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let character = {
+    firstname: "Codemaster",
+    level: "30",
+    class: "warrior",
+    spec: "protection",
+    hometown: "rts",
+}
+
+console.log(character.firstname)//dot notation
+console.log(character["level"]) //bracket notation
